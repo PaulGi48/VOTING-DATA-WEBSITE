45a739831d874a2e601a10cc87ae3815fc0a8ca6
paulgi48 <paulgiovannio@outlook.com>
2022-08-08 15:22:42 +0100

    update

---
+++ include/db.php
@@ -0,0 +1,13 @@
+<?php 
+	$dbserver = "localhost";
+	$dbusername = "root";
+	$dbpass = "";
+	$dbname = "bincom";
+
+	$db = mysqli_connect($dbserver, $dbusername, $dbpass, $dbname);
+	
+	if(!$db){
+	die("Connection Failed". mysqli_errno()."<br />".mysqli_error());
+	
+	}
+?>
